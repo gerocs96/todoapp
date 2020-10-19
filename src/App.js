975e84dda821514c1c055416ea6eab6160b684e4
gerocs96 <gerocs96@gmail.com>
2020-10-19 11:53:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Johnny's list of things to do today</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
+    </div>
+  );
+}
+
+export default App;
